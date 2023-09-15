de6ace8f7ec290b233af02b019b8460fded6f3e0
Nathália Santos <nathsts@motorola.com>
2023-09-15 16:41:52 -0400

    alteração na cor do background

--- gpd-plannig-tool-frontend/src/ui/components/Cabecalho/index.tsx
+++ gpd-plannig-tool-frontend/src/ui/components/Cabecalho/index.tsx
@@ -1,14 +1,14 @@
-import { CabecalhoBarra, CabecalhoLogo } from "./index.style";
+import { CabecalhoBarra, CabecalhoLogo } from './index.style';
 
-import { Container, Link, Toolbar } from "@mui/material";
-import { FC } from "react";
+import { Container, Link, Toolbar } from '@mui/material';
+import { FC } from 'react';
 
 const Cabecalho: FC = (): JSX.Element => {
   return (
-    <CabecalhoBarra position="sticky">
+    <CabecalhoBarra position='sticky'>
       <Toolbar component={Container}>
-        <Link href="/" underline="none">
-          <CabecalhoLogo src="/webacademylogo.png" alt="Web Academy Livros" />
+        <Link href='/' underline='none'>
+          <CabecalhoLogo src='/webacademylogo.png' alt='Web Academy Livros' />
         </Link>
       </Toolbar>
     </CabecalhoBarra>
