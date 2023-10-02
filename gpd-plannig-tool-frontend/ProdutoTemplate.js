a086d2a0f386b46afe64c1665e0fce3d64d70676
Gleides Vinente <gleidevelop@gmail.com>
2023-10-01 23:02:11 -0400

    Add ProdutoTemplate.js como retorno do brackend

---
+++ gpd-plannig-tool-frontend/ProdutoTemplate.js
@@ -0,0 +1,88 @@
+const dadosProduto = {
+    produto01: {
+        id: "ce9d0b3b-9389-4fab-90a9-f79725ad9ac0",
+        name: "Eclipse",
+        sa_date: "02/10/2023",
+        npi_lead: "Name Lead",
+        template: {
+            id: "4a960b5f-4207-466a-a2cd-65bbf91023cd",
+            type: "High",
+            sa_idx: 6,
+            pm: [0, 0.5, 1, 3, 4.2, 4.2, 4.2, 4.2, 4.2],
+            tl: [0, 0.5, 1, 4, 4.2, 4.2, 4.2, 4.2, 4.2],
+            ut_lead: [0, 0, 0, 0, 1, 2, 2, 0, 0],
+        },
+    },
+    produto02: {
+        id: "ce9d0b3b-9389-4fab-90a9-f79725ad9ac0",
+        name: "Beyond",
+        sa_date: "01/11/2023",
+        npi_lead: "Name Lead",
+        template: {
+            id: "4a960b5f-4207-466a-a2cd-65bbf91023cd",
+            type: "Mid",
+            sa_idx: 6,
+            pm: [0, 0.5, 1, 3, 4.2, 4.2, 4.2, 4.2, 4.2],
+            tl: [0, 0.5, 1, 4, 4.2, 4.2, 4.2, 4.2, 4.2],
+            ut_lead: [0, 0, 0, 0, 1, 2, 2, 0, 0],
+        },
+    },
+    produto03: {
+        id: "ce9d0b3b-9389-4fab-90a9-f79725ad9ac0",
+        name: "Lumar",
+        sa_date: "15/12/2023",
+        npi_lead: "Name Lead",
+        template: {
+            id: "4a960b5f-4207-466a-a2cd-65bbf91023cd",
+            type: "Low",
+            sa_idx: 6,
+            pm: [0, 0.5, 1, 3, 4.2, 4.2, 4.2, 4.2, 4.2],
+            tl: [0, 0.5, 1, 4, 4.2, 4.2, 4.2, 4.2, 4.2],
+            ut_lead: [0, 0, 0, 0, 1, 2, 2, 0, 0],
+        },
+    },
+    produto04: {
+        id: "ce9d0b3b-9389-4fab-90a9-f79725ad9ac0",
+        name: "Aurora",
+        sa_date: "04/01/2024",
+        npi_lead: "Name Lead",
+        template: {
+            id: "4a960b5f-4207-466a-a2cd-65bbf91023cd",
+            type: "High",
+            sa_idx: 6,
+            pm: [0, 0.5, 1, 3, 4.2, 4.2, 4.2, 4.2, 4.2],
+            tl: [0, 0.5, 1, 4, 4.2, 4.2, 4.2, 4.2, 4.2],
+            ut_lead: [0, 0, 0, 0, 1, 2, 2, 0, 0],
+        },
+    },
+    produto05: {
+        id: "ce9d0b3b-9389-4fab-90a9-f79725ad9ac0",
+        name: "Amanhecer",
+        sa_date: "16/02/2024",
+        npi_lead: "Name Lead",
+        template: {
+            id: "4a960b5f-4207-466a-a2cd-65bbf91023cd",
+            type: "Mid",
+            sa_idx: 6,
+            pm: [0, 0.5, 1, 3, 4.2, 4.2, 4.2, 4.2, 4.2],
+            tl: [0, 0.5, 1, 4, 4.2, 4.2, 4.2, 4.2, 4.2],
+            ut_lead: [0, 0, 0, 0, 1, 2, 2, 0, 0],
+        },
+    },
+    produto06: {
+        id: "ce9d0b3b-9389-4fab-90a9-f79725ad9ac0",
+        name: "Crepusculo",
+        sa_date: "28/03/2024",
+        npi_lead: "Name Lead",
+        template: {
+            id: "4a960b5f-4207-466a-a2cd-65bbf91023cd",
+            type: "Low",
+            sa_idx: 6,
+            pm: [0, 0.5, 1, 3, 4.2, 4.2, 4.2, 4.2, 4.2],
+            tl: [0, 0.5, 1, 4, 4.2, 4.2, 4.2, 4.2, 4.2],
+            ut_lead: [0, 0, 0, 0, 1, 2, 2, 0, 0],
+        },
+    },
+};
+
+export default dadosProduto;
