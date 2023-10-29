2f1ff13728183e15927632eb25746913bbaaf854
SOLANO <solano.oliveira@icomp.ufam.edu.br>
2023-10-28 22:39:52 -0400

    Sua mensagem de commit

--- gpd-plannig-tool-frontend/src/ui/components/HrmPerMonth/HrmPerMonthForm.tsx
+++ gpd-plannig-tool-frontend/src/ui/components/HrmPerMonth/HrmPerMonthForm.tsx
@@ -1,48 +1,32 @@
 import { Box, Button, Grid, TextField, Typography } from "@mui/material";
-import { useState } from "react";
 import dayjs from "dayjs";
 
 const HrmPerMonthForm = ({ data, updateFieldHandler }) => {
-  // Função para gerar rótulos de meses de acordo com as regras
+
   const generateMonthLabels = () => {
-    const { data_sa, template_type } = data;
+    const { data_sa, template_type, newData } = data;
     const monthLabels = [];
-    const monthNames = [
-      "jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"
-    ];
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    
+    const saDate = dayjs(data_sa);
+    const templateData = newData.find(item => item.template_type === template_type);
 
-    const saDate = dayjs(data_sa); // Converter a data de SA para o formato dayjs
-
-    let numMonthsToShow;
-    let monthsBeforeSA;
-    let monthsAfterSA;
-
-    if (template_type === "high") {
-      numMonthsToShow = 10;
-      monthsBeforeSA = 6;
-      monthsAfterSA = 3;
-    } else if (template_type === "mid") {
-      numMonthsToShow = 4;
-      monthsBeforeSA = 4;
-      monthsAfterSA = 3;
-    } else {
-      numMonthsToShow = 2;
-      monthsBeforeSA = 2;
-      monthsAfterSA = 3;
+    if (!templateData) {
+        return monthLabels;
     }
 
-    const saMonth = saDate.month(); // Mês de SA
-    const saYear = saDate.year(); // Ano de SA
+    const numMonthsToShow = templateData.peak_ammount.split(',').length;
+    const monthsBeforeSA = templateData.sa_idx;
+    const monthsAfterSA = numMonthsToShow - monthsBeforeSA - 1;
+
+    const saMonth = saDate.month();
+    const saYear = saDate.year();
 
     for (let i = -monthsBeforeSA; i <= monthsAfterSA; i++) {
-      const index = (saMonth + i + 12) % 12; // Calcula o índice do mês corrigido para valores negativos
-      const year = saYear + Math.floor((saMonth + i) / 12); // Calcula o ano
-
-      if (i === 0) {
-        monthLabels.push(`${monthNames[index]}`);
-      } else {
-        monthLabels.push(`${monthNames[index]}`);
-      }
+      const index = (saMonth + i + 12) % 12;
+      const year = saYear + Math.floor((saMonth + i) / 12);
+      const label = `${monthNames[index]} ${year}`;
+      monthLabels.push({ label, isSAMonth: i === 0 });
     }
 
     return monthLabels;
@@ -64,40 +48,36 @@ const HrmPerMonthForm = ({ data, updateFieldHandler }) => {
           rowSpacing={1}
           columnSpacing={{ xs: 1, sm: 2, md: 3 }}
         >
-          {data.allocations.map((field, index) => {
-            if (monthLabels[index]) {
-              return (
-                <>
-                  <Grid item xs={3}>
-                    <span>{monthLabels[index]}</span>
-                  </Grid>
-                  <Grid item xs={5}>
-                    <TextField
-                      name={field.month}
-                      label="Allocation"
-                      InputProps={{
-                        readOnly: true,
-                      }}
-                      error
-                      variant="outlined"
-                      fullWidth
-                      value={field.allocation || "0"}
-                      onChange={(e) => updateFieldHandler(e, index)}
-                      size="small"
-                    />
-                  </Grid>
-                  <Grid item xs={4}>
-                    <Button
-                      color="secondary"
-                      sx={{ height: 40, marginRight: 1 }}
-                    >
-                      Update
-                    </Button>
-                  </Grid>
-                </>
-              );
-            }
-          })}
+          {monthLabels.map((labelData, index) => (
+            <>
+              <Grid item xs={3}>
+                <span style={labelData.isSAMonth ? {color: 'blue', fontWeight: 'bold'} : {}}>{labelData.label}</span>
+              </Grid>
+              <Grid item xs={5}>
+                <TextField
+                  name={labelData.label} // aqui foi alterado para usar o label como nome
+                  label="Allocation"
+                  InputProps={{
+                    readOnly: true,
+                  }}
+                  error
+                  variant="outlined"
+                  fullWidth
+                  value={(data.allocations[index] && data.allocations[index].allocation) || "0"}
+                  onChange={(e) => updateFieldHandler(e, index)}
+                  size="small"
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <Button
+                  color="secondary"
+                  sx={{ height: 40, marginRight: 1 }}
+                >
+                  Update
+                </Button>
+              </Grid>
+            </>
+          ))}
         </Grid>
       </Box>
     </>
